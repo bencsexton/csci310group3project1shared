db433feb3ab63329b003d21bba7d2b7950efcee5
Scott Mai <scottmai@usc.edu>
2020-03-11 16:32:36 -0700

    vacation and activity pages mostly done

--- src/main/webapp/js/activityPlanning.js
+++ src/main/webapp/js/activityPlanning.js
@@ -1,11 +1,32 @@
 const resultsTable = $('#results');
 const inputs = ["activity", "numResults", "location"];
 const url = 'http://127.0.0.1:8080/api/activityPlanning/search';
-const tableHeaders = ["City", "Country", "Current Temperature", "Distance", ""];
-const tableDatas = ["city", "country", "avgMinTemp", "avgMaxTemp", "distance"];
+const tableHeaders = ["City", "Country", "Current Temperature"];
+const tableDatas = ["city", "country",  "currentTemp", "distance"];
 const toggleSelector = $('#tempToggle');
 const formSelector = $('#search-form');
+
+const activityInput = $('#activity');
+const activityUrl = 'http://127.0.0.1:8080/api/activityPlanning/activities'
+
+const distanceHeader = $('#distance');
 
 
 initializeToggle(toggleSelector);
 initializeSearch(formSelector, url, resultsTable, inputs, tableHeaders, tableDatas);
+setDistanceIdx(tableDatas);
+// initDistanceSort(distanceHeader, tableDatas, resultsTable);
+
+function initializeAutocomplete(response){
+	$( function() {
+		activityInput.autocomplete({
+			source: response.activities
+		});
+	});
+}
+
+$.get({
+	url: activityUrl,
+	dataType: 'JSON',
+	success: initializeAutocomplete
+});
