7935cb6b2546c068c6d5e79a8a841218485fbf48
bencsexton <bsexton@usc.edu>
2020-03-13 12:24:04 -0700

    Added icon and desc to searchforecast and analysisforecast

--- src/main/java/csci310/model/AnalysisForecast.java
+++ src/main/java/csci310/model/AnalysisForecast.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class AnalysisForecast extends SearchForecast {
 	private List<FutureForecast> next5Days;
 	private List<WeatherHistory> historicalTemps;
-	public AnalysisForecast(String location, Date date, float temp, List<FutureForecast> n5D, List<WeatherHistory> hTs) {
-		super(location, date, temp);
+	public AnalysisForecast(String location, Date date, float temp, List<FutureForecast> n5D, List<WeatherHistory> hTs, String desc, String icon) {
+		super(location, date, temp, desc, icon);
 		next5Days = n5D;
 		historicalTemps = hTs;
 	}
