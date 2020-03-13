729490017c482276a86be54a77812cfcdb966e4c
Scott Mai <scottmai@usc.edu>
2020-03-13 14:01:47 -0700

    fixed cucumber

--- src/test/java/cucumber/RunCucumberTests.java
+++ src/test/java/cucumber/RunCucumberTests.java
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-package cucumber;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-
-/**
- * Run all the cucumber tests in the current package.
- */
-@RunWith(Cucumber.class)
-@CucumberOptions(strict = true, features = { "src/test/resources/cucumber/searchPage.feature" })
-public class RunCucumberTests {
-
-	@BeforeClass
-	public static void setup() {
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-		WebDriverManager.chromedriver().setup();
-	}
-
-}
-||||||| merged common ancestors
-package cucumber;
-
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-
-/**
- * Run all the cucumber tests in the current package.
- */
-@RunWith(Cucumber.class)
-@CucumberOptions(strict = true, features = { "src/test/resources/cucumber/searchPage.feature" })
-@CucumberOptions(strict = true, features = { "src/test/resources/cucumber/hello.feature" })
-public class RunCucumberTests {
-
-	@BeforeClass
-	public static void setup() {
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-		WebDriverManager.chromedriver().setup();
-	}
-
-}
-=======
 package cucumber;
 
 import io.cucumber.junit.Cucumber;
@@ -68,4 +20,3 @@ public class RunCucumberTests {
 	}
 
 }
->>>>>>> b0575581d8cd49955d5338a74e803a71001afaff
