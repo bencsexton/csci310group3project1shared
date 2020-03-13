75934f10d460f677f0b91b8ef7146942db5e9fdb
bencsexton <bsexton@usc.edu>
2020-03-13 13:15:16 -0700

    added haversine distance

--- src/main/java/csci310/WeatherAPIConnector.java
+++ src/main/java/csci310/WeatherAPIConnector.java
@@ -12,6 +12,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+
 import com.google.gson.*;
 
 import csci310.model.AnalysisForecast;
@@ -72,6 +73,18 @@ public class WeatherAPIConnector {
 			connection.disconnect();
 		}
 		return sb.toString();
+	}
+	
+	public static double haversineDistance(double lat1, double lon1, double lat2, double lon2) {
+		double R = 6372.8;
+		double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+ 
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return R * c;
 	}
 	
 	// increment Date by n days
